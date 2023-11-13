3cef962af5f0250d3517b5b781b303254ade32bb
Bastian Schilling <Bastian-Schilling@gmx.de>
2023-11-13 21:16:58 +0100

    working docker compose

--- install.sh
+++ install.sh
@@ -4,17 +4,18 @@ apt install pwgen
 ./generate_secrets.sh
 NOW=$( date '+%F_%H:%M:%S' )
 mkdir ~/pretix-secrets-$NOW
-cp ./secrets/* ~/pretix-secrets-$NOW/
+cp ./secrets/*.txt ~/pretix-secrets-$NOW/
 
 # Pretix
-mkdir /var/pretix-data
+mkdir -p /var/pretix-data
 chown -R 15371:15371 /var/pretix-data
 
-mkdir /etc/pretix
-touch /etc/pretix/pretix.cfg
+mkdir -p /etc/pretix
+#touch /etc/pretix/pretix.cfg
+cp ./pretix.cfg /etc/pretix/pretix.cfg
 chown -R 15371:15371 /etc/pretix/
 chmod 0700 /etc/pretix/pretix.cfg
-cp pretix.cfg /etc/pretix.cfg
+#cp pretix.cfg /etc/pretix.cfg
 
 # Docker
-docker stack deploy -c docker-compose.yml ttl
+docker stack deploy -c docker-compose.yml ttn
