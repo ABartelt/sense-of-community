b7e8c3869f7b86ca4ccaf50f4942815c9da78e8a
Arne Bartelt <arne.bartelt@gmail.com>
2023-11-20 22:04:28 +0100

    Moved install script with permissions on remote host to /opt/.

--- install.sh
+++ install.sh
@@ -1,22 +1,10 @@
 #!/bin/sh
 
-apt install pwgen
-./generate_secrets.sh
-NOW=$( date '+%F_%H:%M:%S' )
-mkdir ./secrets/pretix-secrets-$NOW
-cp ./secrets/*.txt ./secrets/pretix-secrets-$NOW/
-
 # Pretix
-mkdir -p /var/pretix-data
-chown -R 15371:15371 /var/pretix-data
+chown -R 15371:15371 ./pretix/pretix-data
 
-mkdir -p /etc/pretix
-#touch /etc/pretix/pretix.cfg
-cp ./pretix/pretix.cfg /etc/pretix/pretix.cfg
-chown -R 15371:15371 /etc/pretix/
-chmod 0700 /etc/pretix/pretix.cfg
-#cp pretix.cfg /etc/pretix.cfg
+chown -R 15371:15371 ./pretix/etc/
+chmod 0700 ./pretix/etc/pretix.cfg
 
-# Docker
 docker stack deploy -c traefik/docker-compose.yml traefik
 docker stack deploy -c pretix/docker-compose.yml pretix
