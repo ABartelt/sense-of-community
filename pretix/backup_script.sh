0efa5afb9c0d07d0ce831b09a5848f5793f3df30
Arne Bartelt <arne.bartelt@gmail.com>
2023-12-23 00:21:39 +0100

    Fixed some smaller network misses and added postgres backup as sidecar to pretix stack.g

--- pretix/backup_script.sh
+++ pretix/backup_script.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-# Database backup script
+# Check if all required environment variables are set
+if [ -z "$POSTGRES_HOST" ] || [ -z "$POSTGRES_USER" ] || [ -z "$POSTGRES_PASSWORD" ] || [ -z "$POSTGRES_DB" ]; then
+    echo "One or more required environment variables are missing." >&2
+    exit 1
+fi
 
 # Environment variables are passed in from docker-compose
 HOST=$POSTGRES_HOST
@@ -9,11 +13,24 @@ PASSWORD=$POSTGRES_PASSWORD
 DB=$POSTGRES_DB
 BACKUP_DIR=/var/backup
 
-# Backup
-pg_dump -h $HOST -U $USER -d $DB > $BACKUP_DIR/backup_$(date +%Y%m%d_%H%M%S).sql
+# Generate a timestamped backup filename
+BACKUP_FILENAME=backup_$(date +%Y%m%d_%H%M%S).sql
+
+# Perform the backup
+echo "Starting backup for database '$DB'..."
+if PGPASSWORD=$PASSWORD pg_dump -h $HOST -U $USER -d $DB > $BACKUP_DIR/$BACKUP_FILENAME; then
+    echo "Backup successful: $BACKUP_DIR/$BACKUP_FILENAME"
+else
+    echo "Backup failed for database '$DB'" >&2
+    exit 1
+fi
 
 # Delete old backups older than 65 days
+echo "Deleting backups older than 65 days..."
 find $BACKUP_DIR -type f -name '*.sql' -mtime +65 -exec rm {} \;
+
+# Log completion
+echo "Backup process completed."
 
 # Sleep for an hour
 sleep 3600
