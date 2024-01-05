3d9993aed23b26cff721782b7140886362f97644
Arne Bartelt <arne.bartelt@gmail.com>
2024-01-05 23:43:07 +0100

    Fixed replace command by using source for .env vars exposing.

--- replace.sh
+++ replace.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 # Import envs from .env file
-export $(cat .env | grep -v ^# | xargs)
+set -a
+source .env
+set +a
 
 # traefik config
 sed -i '' "s/\${TRAEFIK_LETS_ENCRYPT_EMAIL}/${TRAEFIK_LETS_ENCRYPT_EMAIL}/g" traefik/traefik.yaml
@@ -17,12 +19,13 @@ sudo chmod g+rx pretix/etc/pretix.cfg
 
 # pretix config
 sed -i '' "s/\${PRETIX_POSTGRES_USER}/${PRETIX_POSTGRES_USER}/g" pretix/etc/pretix.cfg
-sed -i ''  "s/\${PRETIX_POSTGRES_DB}/${PRETIX_POSTGRES_DB}/g" pretix/etc/pretix.cfgg
+sed -i ''  "s/\${PRETIX_POSTGRES_DB}/${PRETIX_POSTGRES_DB}/g" pretix/etc/pretix.cfg
 sed -i '' "s/\${PRETIX_POSTGRES_PASSWORD}/${PRETIX_POSTGRES_PASSWORD}/g" pretix/etc/pretix.cfg
 sed -i '' "s/\${PRETIX_MAIL_HOST}/${PRETIX_MAIL_HOST}/g" pretix/etc/pretix.cfg
 sed -i '' "s/\${PRETIX_MAIL_USER}/${PRETIX_MAIL_USER}/g" pretix/etc/pretix.cfg
 sed -i '' "s/\${PRETIX_MAIL_PASSWORD}/${PRETIX_MAIL_PASSWORD}/g" pretix/etc/pretix.cfg
 
 sed -i '' "s/\${PRETIX_HOSTNAME}/${PRETIX_HOSTNAME} /g" pretix/etc/pretix.cfg
+
 sed -i '' "s/\${PRETIX_INSTANCE_NAME}/${PRETIX_INSTANCE_NAME}/g" pretix/etc/pretix.cfg
 sed -i '' "s/\${PRETIX_MAIL_PORT}/${PRETIX_MAIL_PORT}/g" pretix/etc/pretix.cfg
